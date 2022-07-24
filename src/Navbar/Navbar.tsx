f5d97273f6e84978ab9dadefbf8ae8ca4afc9eb7
Sergey Zuev <ekzaton@gmail.com>
2022-07-24 20:02:26 +0500

    2. Added Navbar component

---
+++ src/Navbar/Navbar.tsx
@@ -0,0 +1,27 @@
+import {StyleSheet, Text, View} from 'react-native';
+
+import {NavbarProps} from "./Navbar.props";
+
+export default function Navbar(props: NavbarProps) {
+  const { title } = props;
+
+  return (
+      <View style={styles.navbar}>
+        <Text style={styles.text}>{title}</Text>
+      </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  navbar: {
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: 70,
+    paddingBottom: 10,
+    backgroundColor: '#3949AB',
+  },
+  text: {
+    fontSize: 20,
+    color: 'white',
+  }
+});
